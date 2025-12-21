c4311bc44de779b0aeabf1fd23d72b98237c54ea
Shidharth Sharma <nigaa304@gmail.com>
2025-12-21 13:03:55 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -35,7 +35,8 @@ const Navbar = () => {
       <div className="container mx-auto px-4 md:px-8">
         <div className="flex items-center justify-between">
           {/* Logo */}
-          <Link to="/" className="flex items-center group bg-white rounded-xl px-3 py-2 shadow-soft">
+          <Link to="/" className="flex items-center group">
+
             <img 
               src={hapidaLogo} 
               alt="HAPIDA SKY Private Limited" 
